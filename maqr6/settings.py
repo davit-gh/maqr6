b1a536f5c727d417232b019ee5c523fcb953e455
davit-gh <davsmile@yahoo.com>
2017-05-12 15:58:59 +0400

    remove STATICFILES_DIR setting

--- maqr6/settings.py
+++ maqr6/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
